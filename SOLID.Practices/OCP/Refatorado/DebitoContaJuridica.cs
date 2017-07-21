dc70990fff9ff440256c0263bd6942b02a6b8b08
Eduardo Conz <eduardo.conz@hbsis.com.br>
2017-07-21 10:05:41 -0300

    melhorias

---
+++ SOLID.Practices/OCP/Refatorado/DebitoContaJuridica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Practices.OCP.Refatorado
+{
+    public class DebitoContaJuridica : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
